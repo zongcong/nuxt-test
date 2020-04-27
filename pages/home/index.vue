469da19e022ed32d6482201e95a7c9ecaf61a6aa
zongcong <tanzongcong@qkagame.com>
2020-04-27 16:58:47 +0800

    第一次提交

---
+++ pages/home/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in obj" :key="item.id" style="padding: 10px 20px">
+        <p>{{ item.title }}</p>
+        <p v-html="item.content_rendered" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $axios }) {
+    const obj = await $axios.$get('/api/topics/hot.json')
+    return { obj }
+  },
+  // data () {
+  //   return {
+  //     obj: [{ title: '11' }]
+  //   }
+  // },
+  head () {
+    return {
+      title: `Page 1 (${this.obj[0].title}-side)`,
+      meta: [
+        { hid: 'description', name: 'description', content: 'Page 1 description' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
